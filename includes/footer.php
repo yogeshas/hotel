0f1480ed256bffd1a59b8de84daaf0705e82938d
yogeshas <51391191+yogeshas@users.noreply.github.com>
2023-11-11 13:35:41 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,10 @@
+<br>
+<footer class="footer">
+
+	<div class="card">
+		<div class="card-body">
+			<p class="d-flex justify-content-center">@c yogesh. </p>
+		</div>
+	</div>
+	
+</footer>
---
+++ includes/header.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Hotel Mangement</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body >
+  <nav class="navbar navbar-expand-lg navbar-dark">
+  
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item ">
+        <a class="nav-link text-white" href="index">Home </a>
+      </li>
+      <li class="nav-item ">
+        <a class="nav-link text-white" href="pages/contact">Contact</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-white" href="pages/about">About</a>
+      </li>
+
+  </ul>
+  </div> 
+  <a class="navbar-brand" href="#" id="logo" data-toggle="collapse">&nbsp;&nbsp;&nbsp;DIH &nbsp;</a>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a class="nav-link text-white" href="pages/register">Register</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-white" href="pages/login">Login</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-white" href="pages/pricing">Pricing</a>
+      </li>
+      
+     
+     
+     
+    </ul>
+    
+  </div>
+</nav>
