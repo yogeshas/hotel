41304df745d3cfc173d66b2519b05f7704f88e6b
yogeshas <51391191+yogeshas@users.noreply.github.com>
2023-11-11 13:21:38 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,44 @@
+
+<?php
+
+include 'includes/header.php';
+
+?>
+
+<div class="row">
+	
+	
+
+	<div class="col-lg-6">
+		<div class="container-fluid" style="margin-top: 100px;">
+			<h1 class="text-white text-fluid">
+				<b>Welcome To Food Order System.  </b>
+			</h1><br>
+			<h6 class="text-white text-fluid">
+				The Application is built for the hotel. the application will help you in <br>
+				ordering the food from online <a href="pages/about" style="color: orange;" > click here </a> to learn more. 
+
+			</h6>
+			<br>
+				<form action="booking.php">
+					<button class="btn btn-danger">BOOK A DEMO ON APPLICATION</button>
+				</form>
+			
+		</div>
+	</div>
+	<div class="col-lg-6">
+		<div class="container-fluid">
+		<img src="https://www.zuyyu.in/static/website/img/feature/digital-menu.png" class="img-fluid" id="img_adj" >
+		</div>
+		
+		
+
+	
+			
+		</div>
+
+</div>
+
+
+</body>
+</html>
